f3af5991b28394ccbc96fec2377dde1dd09037ca
dilrajkingra <kingradilraj@gmail.com>
2026-01-01 20:00:00 -0700

    Make all LEDs blink

--- app/src/main.c
+++ app/src/main.c
@@ -1,30 +1,41 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 
-/* LED0 alias comes from the nRF52840DK device tree */
-static const struct gpio_dt_spec led =
+/* LED aliases defined in the nRF52840DK device tree */
+static const struct gpio_dt_spec led0 =
     GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+static const struct gpio_dt_spec led1 =
+    GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
+static const struct gpio_dt_spec led2 =
+    GPIO_DT_SPEC_GET(DT_ALIAS(led2), gpios);
+static const struct gpio_dt_spec led3 =
+    GPIO_DT_SPEC_GET(DT_ALIAS(led3), gpios);
 
 int main(void)
 {
-    int ret;
-
-    if (!gpio_is_ready_dt(&led)) {
+    /* Ensure all LEDs are ready */
+    if (!gpio_is_ready_dt(&led0) ||
+        !gpio_is_ready_dt(&led1) ||
+        !gpio_is_ready_dt(&led2) ||
+        !gpio_is_ready_dt(&led3)) {
         return 0;
     }
 
-    /* Active-low LED: inactive = off */
-    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
-    if (ret < 0) {
-        return 0;
-    }
+    /* Configure LEDs as outputs (active-low, start OFF) */
+    gpio_pin_configure_dt(&led0, GPIO_OUTPUT_INACTIVE);
+    gpio_pin_configure_dt(&led1, GPIO_OUTPUT_INACTIVE);
+    gpio_pin_configure_dt(&led2, GPIO_OUTPUT_INACTIVE);
+    gpio_pin_configure_dt(&led3, GPIO_OUTPUT_INACTIVE);
 
     while (1) {
-        /* blinking comes next */
-        gpio_pin_toggle_dt(&led);
-        k_msleep(500);
+        gpio_pin_toggle_dt(&led0);
+        gpio_pin_toggle_dt(&led1);
+        gpio_pin_toggle_dt(&led2);
+        gpio_pin_toggle_dt(&led3);
 
+        k_msleep(500);
     }
 
     return 0;
 }
+
