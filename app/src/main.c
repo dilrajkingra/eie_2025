c248b9b7a730752f80aca53ab6515b75d0c61bbc
dilrajkingra <kingradilraj@gmail.com>
2026-01-01 19:46:32 -0700

    Initialize LED pin

--- app/src/main.c
+++ app/src/main.c
@@ -1,11 +1,27 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 
+/* LED0 alias comes from the nRF52840DK device tree */
+static const struct gpio_dt_spec led =
+    GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+
 int main(void)
 {
-    while (1) {
-        // nothing yet
+    int ret;
+
+    if (!gpio_is_ready_dt(&led)) {
+        return 0;
     }
-    
+
+    /* Active-low LED: inactive = off */
+    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
+    if (ret < 0) {
+        return 0;
+    }
+
+    while (1) {
+        /* blinking comes next */
+    }
+
     return 0;
 }
